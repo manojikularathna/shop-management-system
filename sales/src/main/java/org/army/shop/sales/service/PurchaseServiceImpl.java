b6ffc0c27b2635841195e4d1068ff3aaa77a7bfd
chirantha <chirantha@calcey.com>
2018-09-25 07:11:39 +0530

    bl impl

---
+++ sales/src/main/java/org/army/shop/sales/service/PurchaseServiceImpl.java
@@ -0,0 +1,26 @@
+package org.army.shop.sales.service;
+
+import org.army.shop.sales.api.PurchaseService;
+import org.army.shop.sales.to.BaseResponse;
+import org.army.shop.sales.to.CancelTransactionRequest;
+import org.army.shop.sales.to.PurchaseRequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/purchase")
+public class PurchaseServiceImpl implements PurchaseService {
+
+    @RequestMapping(path = "/", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public BaseResponse purchase(@RequestBody PurchaseRequest purchaseOrder) {
+        return null;
+    }
+
+    @RequestMapping(path = "/cancel", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public BaseResponse cancelPurchase(@RequestBody CancelTransactionRequest cancel) {
+        return null;
+    }
+}
--- sales/src/main/java/org/army/shop/sales/service/SalesServiceImpl.java
+++ sales/src/main/java/org/army/shop/sales/service/SalesServiceImpl.java
@@ -1,6 +1,10 @@
 package org.army.shop.sales.service;
 
+import org.army.shop.sales.api.SalesService;
 import org.army.shop.sales.bl.SalesServiceBL;
+import org.army.shop.sales.to.BaseResponse;
+import org.army.shop.sales.to.CancelTransactionRequest;
+import org.army.shop.sales.to.SalesCalculationResponse;
 import org.army.shop.sales.to.SalesRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,10 +14,27 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping(path = "/inventory")
-public class SalesServiceImpl {
+@RequestMapping(path = "/sale")
+public class SalesServiceImpl implements SalesService {
 
 	@Autowired
 	SalesServiceBL salesServiceBL;
 
+	@RequestMapping(path = "/calculate", method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public SalesCalculationResponse calculate(@RequestBody SalesRequest sales) {
+		return null;
+	}
+
+	@RequestMapping(path = "/", method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public BaseResponse sale(@RequestBody SalesRequest sales) {
+		return null;
+	}
+
+	@RequestMapping(path = "/cancel", method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public BaseResponse cancelSale(@RequestBody CancelTransactionRequest cancel) {
+		return null;
+	}
 }
